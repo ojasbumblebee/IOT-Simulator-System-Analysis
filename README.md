c15de4aa796a6932edcb4a94aad78c4b714498a6
ovbarve <ovbarve@ncsu.edu>
2018-09-13 15:54:52 -0400

    initial working code with exponential time arrivals

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # IOT_project_01
+
+
+Launch Script
+
+```
+./task_2.sh
+```
+
+set command line arguments accordingly
+
+```
+python3 task_2.py --mean_inter_arrival_time 6 \n
+		  --mean_orbitting_time 5 \n
+		  --service_time 10 \n
+		  --buffer_size 2 \n
+		  --end_simulation_time 200
+```
+
--- random_number_generator.py
+++ random_number_generator.py
@@ -1,17 +1,30 @@
-def seed(x):
-    global xi
-    xi = x
+import math
 
-def rng():
-    global xi
-    xi = (a*xi + c)%m
-    return xi
 
-a = 3
-c = 9
-m = 16
-xi = 0
+# helper function to generate psuedo random
+def rng(xi, a, c, m):
+    new_xi = (a*xi+c)%m
+    return new_xi
 
-for i in range(10):
-    print (rng())
-                                
+
+#helper function to generate random_variate
+#return exponentially  generated random variate
+def random_variate_generator(lambda_mean, psuedo_random_number):
+    logarithm = math.log(psuedo_random_number)
+    random_variate = -logarithm/lambda_mean
+    return random_variate
+
+
+#main_function 
+def random_number_main(lambda_mean, seed=0.1, a=3, c=9, m=16):
+    
+    psuedo_random_number = rng(seed, a, c, m)
+    random_variate = random_variate_generator(lambda_mean, psuedo_random_number)
+    return random_variate, psuedo_random_number
+
+
+
+random_variate, psuedo_random_number = random_number_main(lambda_mean=6)
+random_variate, psuedo_random_number = random_number_main(lambda_mean=5)
+
+print(random_variate, psuedo_random_number)
